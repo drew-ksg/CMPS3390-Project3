54fe787286df93c7f9f88fbf96a799592b1d35d7
andrew <arivera8127@gmail.com>
2025-11-16 20:55:07 -0800

    got requirements file out of virtual env and got config file set up

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,1 +1,18 @@
 # TODO: Load settings from .env using Pydantic BaseSettings (SECRET_KEY, DATABASE_URL)
+from pydantic_settings import BaseSettings
+from functools import lru_cache
+
+class Settings(BaseSettings):
+    Database_URL: str
+    API_KEY: str
+    SECRET_KEY: str
+    Algorithm: str = "H256"
+    Access_Token_Expire_Minutes: int = 30
+    class Config:
+        env_file = ".env"
+
+@lru_cache()
+def get_settings() -> Settings:
+    return Settings()
+
+
