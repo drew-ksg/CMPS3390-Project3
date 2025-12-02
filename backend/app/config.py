3cc2b66fc185db2cb5bc955ba02472b3f5111271
drew-ksg <arivera8127@gmail.com>
2025-12-01 19:17:40 -0800

    added new package to requirements.txt and fixed error loading .env file

--- backend/app/config.py
+++ backend/app/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     class Config:
-        env_file = ".env"
+        env_file = "app/.env"
 
 @lru_cache()
 def get_settings() -> Settings:
