af88c78d004bedc4425ca3179a39fe64ab33d080
andrew <arivera8127@gmail.com>
2025-12-02 16:13:03 -0800

    hopefully config works

--- backend/app/config.py
+++ backend/app/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     class Config:
-        env_file = str(Path(__file__).resolve().parent / ".env")
+         env_file = str(Path(__file__).resolve().parent / ".env")
 
 @lru_cache()
 def get_settings() -> Settings:
